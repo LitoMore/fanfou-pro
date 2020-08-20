80d7e60b97a6ed903f8068fa14a2900eb65d7936
LitoMore <litomore@gmail.com>
2020-08-20 11:59:06 +0800

    Move API settings to `.env`

--- src/api/index.js
+++ src/api/index.js
@@ -6,13 +6,17 @@ export const consumerSecret = '1afe9c453d6d47c641e00b50690aba51';
 export const oauthToken = localStorage.getItem('fanfouProToken');
 export const oauthTokenSecret = localStorage.getItem('fanfouProTokenSecret');
 
+const {REACT_APP_API_DOMAIN, REACT_APP_OAUTH_DOMAIN} = process.env;
+
 export const ff = new Fanfou({
 	consumerKey,
 	consumerSecret,
-	apiDomain: 'cors.fanfou.pro',
-	oauthDomain: 'cors.fanfou.pro',
+	apiDomain: REACT_APP_API_DOMAIN,
+	oauthDomain: REACT_APP_OAUTH_DOMAIN,
 	hooks: {
 		baseString: string => string
+			.replace('%2F%2Fcors2.fanfou.pro%2Foauth', 'http%3A%2F%2Ffanfou.com%2Foauth')
+			.replace('%2F%2Fcors2.fanfou.pro', 'http%3A%2F%2Fapi.fanfou.com')
 			.replace('%2F%2Fcors.fanfou.pro%2Foauth', 'http%3A%2F%2Ffanfou.com%2Foauth')
 			.replace('%2F%2Fcors.fanfou.pro', 'http%3A%2F%2Fapi.fanfou.com')
 	}
